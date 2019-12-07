814756afa4e962a581744c80c3f216553b63ccb1
Tiur Mawaddah <tiurzm@gmail.com>
2019-12-06 18:37:20 -0600

    finish

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,5 @@
 const friends = [{
-    routeName: "friend1",
-    name: "totoro",
+    name: "Totoro",
     photo: "https://ctl.s6img.com/society6/img/V7IT1SODyRFyoqKJXwyV7xHuQpQ/w_700/prints/~artwork/s6-0093/a/36061447_9991764/~~/totoro-zh2-prints.jpg",
     scores:[
         5,
@@ -15,37 +14,65 @@ const friends = [{
         1
       ]
 }, {
-    routeName: "friend2",
-    name: "mei",
+    name: "Mei",
     photo: "https://d1m6vmmwsgiy3l.cloudfront.net/images/screenshots/screenshot_7_13766.jpg",
     scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
+        3,
         1,
         2,
-        5,
         4,
-        1
+        2,
+        5,
+        1,
+        3,
+        2,
+        5
       ]
 }, {
-    routeName: "friend3",
-    name: "satsuki",
+    name: "Satsuki",
     photo: "https://vignette.wikia.nocookie.net/disney/images/b/b9/Satsuki.jpg/revision/latest?cb=20140725154339",
     scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
+        3,
         2,
         5,
+        1,
+        1,
         4,
-        1
+        2,
+        2,
+        3,
+        5
       ]
+}, {
+name: "No face",
+  photo: "https://i.pinimg.com/originals/ff/54/8c/ff548ce6d6fe1dba69a5d325f7a7a3c3.jpg",
+  scores:[
+      3,
+      1,
+      2,
+      4,
+      2,
+      5,
+      1,
+      4,
+      2,
+      3
+    ]
+}, {
+name: "Haku",
+  photo: "https://vignette.wikia.nocookie.net/disney/images/9/90/Haku.png/revision/latest?cb=20131201071214",
+  scores:[
+      3,
+      2,
+      5,
+      1,
+      1,
+      4,
+      2,
+      2,
+      3,
+      5
+    ]
 }];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,30 +1,42 @@
-const friends = require("../data/friends")
+const friends = require("../data/friends");
 
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
-    return res.json(friends);
-  });
-
-  app.get("/api/friends/:friend", function(req, res) {
-    let chosen = req.params.friend;
-    console.log(chosen);
-    for (let i = 0; i < friends.length; i++) {
-      if (chosen === friends[i].routeName) {
-        return res.json(friends[i]);
-      }
-    }
-  return res.json(false);
-  
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
-    let newFriend = req.body;
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    console.log(newFriend);
-    friends.push(newFriend);
-    res.json(newFriend);
-  });
+    let bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
 
+    let userData =  req.body;
+    let userScores = userData.scores;
+    let totalDifference;
+
+    for(let i = 0; i < friends.length; i++){
+      let currentFriend = friends[i]; 
+      totalDifference = 0;
+     
+      let length = currentFriend.scores.length
+      for(let j = 0; j < length; j++){
+        let currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+      if (totalDifference <= bestMatch.friendDifference) {
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+    
+    friends.push(userData);
+    res.json(bestMatch);
+  });
 };

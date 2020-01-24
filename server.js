65211ad02c28cb3f900f3db489c7d74e991d6d73
Tiur Mawaddah <tiurzm@gmail.com>
2020-01-23 19:33:39 -0600

    added background image

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// important for background image
 app.use(express.static("./app/public"));
 
 require("./app/routing/apiRoutes")(app);
